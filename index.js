e34e4e62f27ab4e906e7683e8c009ef1c357c061
Abir Khan <abirkhan86160@gmail.com>
2026-02-01 23:08:45 +0600

    javascript live class 1

---
+++ index.js
@@ -0,0 +1,74 @@
+
+
+console.log("Hello, World!");
+
+       
+             var x=10;
+             var y=20;
+             var z=x+y;
+            console.log(z);
+
+
+             var a=5;
+             var b=15;
+             var c=a+b;
+             console.log(c);
+
+
+             //object example
+             var person={
+                name:"John",
+                age:30,
+                city:"New York",
+                isbangladeshi:true,
+                car:null,
+             };
+             console.log(person.isbangladeshi);
+
+
+             //array
+
+                var fruits=["apple","banana","orange","mango"];
+
+                console.log(fruits);
+
+
+
+                      var name="Abir khan";
+        var age=25;
+        var yourcitykushtia=true;
+
+        var yourname
+        var youhavemoney=null;
+        console.log(name);
+        console.log(age);
+        console.log(yourcitykushtia);
+        console.log(yourname);
+        console.log(youhavemoney);
+
+
+
+        //object
+
+
+        var person={
+            name:"Abir khan",
+            age:25,
+            city:"kushtia"
+        };
+        console.log(person.age);
+
+
+           //array
+
+        var fruits=["Mango","Banana","Orange","Apple"];
+        console.log(fruits);
+        console.log(fruits[2]);
+        
+
+        
+
+
+        
+
+             
